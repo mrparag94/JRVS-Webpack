fad8c413ee23898d5f1bb69ce1f344c67d4e2f50
mrparag94 <m.r.parag94@gmail.com>
2021-01-05 14:28:16 +0600

    Fix image, video and font loader

--- webpack.config.js
+++ webpack.config.js
@@ -128,20 +128,36 @@ const config = {
                     ]
                 }
             },
+            // {
+            //     test: /\.svg$/,
+            //     use: 'file-loader'
+            // },
+            // {
+            //     test: /\.png$/,
+            //     use: [
+            //         {
+            //             loader: 'url-loader',
+            //             options: {
+            //                 mimetype: 'image/png'
+            //             }
+            //         }
+            //     ]
+            // },
             {
-                test: /\.svg$/,
-                use: 'file-loader'
+                test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: 'assets/images',
+                }
             },
             {
-                test: /\.png$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            mimetype: 'image/png'
-                        }
-                    }
-                ]
+                test: /\.(webm|mp4)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: 'assets/videos',
+                }
             },
             {
                 test: /\.svelte$/,
@@ -163,7 +179,7 @@ const config = {
                 ]
             },
             {
-                test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
                 loader: 'file-loader',
                 options: {
                     name: '[name].[ext]',
@@ -243,10 +259,10 @@ const config = {
     },
     devtool: 'cheap-module-source-map',
     devServer: {
-        contentBase: path.join(__dirname, 'src'),
+        contentBase: path.join(__dirname, 'dist'),
         hot: true,
         compress: true,
-        open: true,
+        open: false,
         historyApiFallback: true,
         port: 5678
     }
